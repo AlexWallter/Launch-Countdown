e1b484ea2a2c0c44d515e602b01d038ece42f1aa
AlexWallter <130161232+AlexWallter@users.noreply.github.com>
2023-06-26 11:36:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,15 +28,40 @@ function countDown() {
         let arr2 = arr1.map(item => item<10?item=`0${item}`:item=item)
 
 
-        daysId.innerHTML = `<div class = "time-container"> <h2>${arr2[0]}</h2> </div> <p class = "time-counter">DAYS</p>`
-        hoursId.innerHTML = `<div class = "time-container"> <h2>${arr2[1]}</h2> </div> <p class = "time-counter">HOURS</p>`
-        minutesId.innerHTML = `<div class = "time-container"> <h2>${arr2[2]}</h2> </div>  <p class = "time-counter">MINUTES</p>`
-        secondsId.innerHTML = `<div class = "time-container"> <h2>${arr2[3]}</h2> </div> <p class = "time-counter">SECONDS</p>`
-    } 
-    else {
-        return alert('its been launched!')
-    }
+        daysId.innerHTML = `<div class = "time-container"> <div id="flipDays-card"><h2>${arr2[0]}</h2></div> <h2>${arr2[0]}</h2> </div> <p class = "time-counter">DAYS</p>`
+        hoursId.innerHTML = `<div class = "time-container"> <div id="flipHours-card"><h2>${arr2[1]}</h2></div> <h2>${arr2[1]}</h2> </div> <p class = "time-counter">HOURS</p>`
+        minutesId.innerHTML = `<div class = "time-container"> <div id="flipMin-card"><h2>${arr2[2]}</h2></div> <h2>${arr2[2]}</h2> </div>  <p class = "time-counter">MINUTES</p>`
+        secondsId.innerHTML = `<div class = "time-container"> <div id="flip-card" class = "active"><h2>${arr2[3]}</h2></div> <h2>${arr2[3]}</h2> </div> <p class = "time-counter">SECONDS</p>`
 
+        const flipMin = document.querySelector('#flipMin-card')
+        const flipHours = document.querySelector('#flipHours-card')
+        const flipDays = document.querySelector('#flipDays-card')
+
+
+        if(arr2[3]==0){
+            flipMin.classList.add('active')
+        } 
+        else {
+            flipMin.classList.remove('active')
+        }
+
+        if(arr2[2]==0){
+            flipHours.classList.add('active')
+        } 
+        else {
+            flipHours.classList.remove('active')
+        }
+
+        if(arr2[1]==0){
+            flipDays.classList.add('active')
+        } 
+        else {
+            flipDays.classList.remove('active')
+        }
+    }
+    else {
+        return alert('it`s been launched!')
+    }
 }
 
 setInterval(countDown, 1000)
